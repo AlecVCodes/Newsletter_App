4cb8066decdbebf417ea95931d1d7233932b9e2e
AlecVCodes <alectvine@gmail.com>
2025-10-17 19:53:50 +1000

    success message
    user success message commit

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const newsletterForm = document.querySelector(".newsletter-form");
 const formSubmitBtn = document.querySelector(".submit-btn");
 const input = document.querySelector(".email-data");
-console.log(newsletterForm, formSubmitBtn);
+
+// success container
+
+// success container email text
+const successContainerEmail = document.getElementById("user-email");
+
+console.log(successContainerEmail, "dsafkmsdkfamsk");
 
 function handleSubmit(e) {
   // prevent the browser from loading and automatically submitting form
@@ -15,7 +21,10 @@ function handleSubmit(e) {
 
   if (data.name === "") {
     console.log("empty name");
+  } else {
+    successContainerEmail.innerText = input.value;
   }
+
   console.log(data);
 }
 
